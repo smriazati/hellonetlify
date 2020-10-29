d10b2746bcb03a245a8d29645104634b0468e731
= <=>
2020-10-28 23:46:00 -0400

    update to video data

--- store/index.js
+++ store/index.js
@@ -1,33 +1,41 @@
 import gql from "graphql-tag";
 
 export const state = () => ({
-    favoriteGames: []
+    siteData: []
 });
 
 export const actions = {
     async callApollo({ commit }) {
       let response = await this.app.apolloProvider.defaultClient.query({
-        query: gql`query GetGames {
-            favoriteGames {
-              id
-              image
-              name
-              rating
-              gameUser {
-                id
-                name
-                profileImg
-              }
-            }
-          }`
+        // query: gql`query GetGames {
+        //     favoriteGames {
+        //       id
+        //       image
+        //       name
+        //       rating
+        //       gameUser {
+        //         id
+        //         name
+        //         profileImg
+        //       }
+        //     }
+        //   }`
+        query: gql`query getVids {
+          threeByThirteenProject {
+             videoId
+            videoUrl
+            title
+            img
+          }
+        }`
       });
    
-      await commit("updateFavoriteGames", response.data.favoriteGames);
+      await commit("updateData", response.data.siteData);
     }
 };
 
 export const mutations = {
-    updateFavoriteGames: (state, data) => {
-      state.favoriteGames = data;
+    updateData: (state, data) => {
+      state.siteData = data;
     }
 };
